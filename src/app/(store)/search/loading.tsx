8d4fbcf1edc160f9440eae4493a29fe39f4d3e9e
Luiz Thiago <thiago_fers7@hotmail.com>
2024-05-13 23:51:58 -0300

    Add loading page for search products

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,3 +1,24 @@
+'use client'
+
+import { Skeleton } from '@/components/skeleton'
+import { useSearchParams } from 'next/navigation'
+
 export default function Loading() {
-  return <div>Loading...</div>
+  const searchParams = useSearchParams()
+
+  const query = searchParams.get('q')
+
+  return (
+    <div className="flex flex-col gap-4">
+      <p className="text-sm">
+        Resultados para <span className="font-semibold">{query}</span>
+      </p>
+
+      <div className="grid grid-cols-3 gap-6">
+        {new Array(6).fill(null).map((_, index) => (
+          <Skeleton key={index} className="h-[430px]" />
+        ))}
+      </div>
+    </div>
+  )
 }
